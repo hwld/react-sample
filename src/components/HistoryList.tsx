52d8f9096a31b5ec49321f8fbf1df64e09b1836f
hwld <0701hw@gmail.com>
2020-01-15 14:21:26 +0900

    着手履歴の並び順を設定可能にした

--- src/components/HistoryList.tsx
+++ src/components/HistoryList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export interface HistoryItem {
   squares: string[] | null[];
@@ -17,6 +17,8 @@ const HistoryList: React.FC<HistoryListProps> = ({
   stepNum,
   jumpTo,
 }) => {
+  const [order, setOrder] = useState('asc');
+
   const moves = history.map((step, move) => {
     const desc = move
       ? `Go to move #{col: ${step.hand.col} , row: ${step.hand.row}}`
@@ -35,7 +37,41 @@ const HistoryList: React.FC<HistoryListProps> = ({
     );
   });
 
-  return <ol>{moves}</ol>;
+  return (
+    <>
+      <label htmlFor="asc">
+        <input
+          type="radio"
+          value="asc"
+          id="asc"
+          name="order"
+          checked={order === 'asc'}
+          onChange={event => {
+            setOrder(event.target.value);
+          }}
+        />
+        昇順
+      </label>
+
+      <label htmlFor="desc">
+        <input
+          type="radio"
+          value="desc"
+          id="desc"
+          name="order"
+          checked={order === 'desc'}
+          onChange={event => {
+            setOrder(event.target.value);
+          }}
+        />
+        降順
+      </label>
+
+      <ol className={order === 'asc' ? 'history-list' : 'history-list-reverse'}>
+        {moves}
+      </ol>
+    </>
+  );
 };
 
 export default HistoryList;
